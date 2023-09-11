0492adb6d94e9731375479f1685807ace58810af
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-11 21:56:04 +0530

    commit5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./styles.css";
 import NavBar from "./Components/nav-bar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Services from "./routes/Services";
+import Contact from "./routes/Contact";
+
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Services" element={<Services/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
       </Routes>
       <NavBar></NavBar>
       
