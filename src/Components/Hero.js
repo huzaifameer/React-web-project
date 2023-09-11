20df07f2a37fdc081fceb61e99b30b90afa46bc0
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-11 22:44:26 +0530

    commit8

---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+import "./Hero-styles.css";
+
+function Hero() {
+    return (
+        <>
+        <div className="hero">
+            <img alt="Hero Image" src="/Assets/HomePage.png" />
+        </div>
+        </>
+    )
+}
+export default Contact;
