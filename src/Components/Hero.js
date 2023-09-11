edf9a4602e66b2c8eb25bf86cdd0a17b98020a8c
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-11 23:00:36 +0530

    commit9

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,9 +4,14 @@ function Hero() {
     return (
         <>
         <div className="hero">
-            <img alt="Hero Image" src="/Assets/HomePage.png" />
+            <img alt="Hero-Image" src="https://img.freepik.com/premium-photo/cup-coffee-beansgenerative-ai_391052-22577.jpg?w=1060" />
+            <div className="hero-text">
+                <h1>Begin your journey With Us</h1>
+                <p>Providing a better service</p>
+                <a href="#">Our Plan</a>
+            </div>
         </div>
         </>
     )
 }
-export default Contact;
+export default Hero;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,10 @@
 import NavBar from "../Components/nav-bar";
+import Hero from "../Components/Hero";
 function Home() {
     return (
         <>
-            <NavBar/>
-            <h1>This is the Home Page</h1>
+        <NavBar/>
+        <Hero/>
         </>
     )
 }
