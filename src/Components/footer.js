2c08e85488a494906844aa25f0689c7768580cd1
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-13 23:23:18 +0530

    commit28

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,10 +3,53 @@ const Footer=()=>{
     return(
         <div className="footer">
             <div className="top">
-                <div><h1>So<u>ll</u>dZ Solutions</h1></div>    
-                <div></div> 
+                <div>
+                    <h1>So<u>ll</u>dZ Solutions</h1>
+                    <p>Chase your dreams with us.</p>
+                </div>    
+                <div>
+                    <a href="#">
+                        <i className="fa-brands fa-facebook-square"></i>
+                    </a>
+                    <a href="">
+                        <i className="fa-brands fa-instagram-square"></i>
+                    </a>
+                    <a href="">
+                        <i className="fa-brands fa-linkedin"></i>
+                    </a>
+                    <a href="">
+                        <i className="fa-brands fa-twitter-square"></i>
+                    </a>
+                </div> 
             </div> 
-            <div className="bottom"></div>         
+            <div className="bottom">
+                <div>
+                    <h4>Project</h4>
+                    <a href="#">ChangeLog</a>
+                    <a href="#">Status</a>
+                    <a href="#">License</a>
+                    <a href="#">All Versions</a>
+                </div>
+                <div>
+                    <h4>Help</h4>
+                    <a href="#">Support</a>
+                    <a href="#">TroubleShooting</a>
+                    <a href="#">Contact Us</a>
+                </div> 
+                <div>
+                    <h4>Community</h4>
+                    <a href="#">GitHub</a>
+                    <a href="#">Issues</a>
+                    <a href="#">Projects</a>
+                    <a href="#">Twitter</a>
+                </div> 
+                <div>
+                    <h4>Others</h4>
+                    <a href="#">Terms of service</a>
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">License</a>
+                </div>         
+            </div>         
         </div> 
     )
 }
